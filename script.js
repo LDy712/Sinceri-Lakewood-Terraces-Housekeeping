4db811f52bf59413573fa1687c2891fda0094a2c
LDy712 <erinldy@outlook.com>
2026-05-27 22:32:34 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,67 @@
 // "Database" (data layer)
 const roomData = {
-  "Living Room": {
-    title: "Living Room",
-    description: "Main common area for residents.",
-    status: "Available"
+  "1301": {
+    title: "ROOM 1301",
+    Resident: "RESIDENT NAME: Unknown",
+    Notes: "NOTE: Prefers no housekeeping",
+    Dining: "Dining Preference: Eats downstairs"
+
+  },
+
+  "1302": {
+    title: "ROOM 1302",
+    Resident: "RESIDENT NAME: Mary Anne McGovern",
+    Notes: "NOTE: Very clean room, cleans on her own time.",
+    Dining: "Dining Preference: Eats downstairs"
+  },
+
+  "1303": {
+    title: "ROOM 1303",
+    Resident: "RESIDENT NAME: EMPTY ROOM",
+    Notes: "NOTE: N/A",
+    Dining: "Dining Preference: N/A"    
+  },
+
+  "1304": {
+    title: "ROOM 1304",
+    Resident: "RESIDENT NAME: Patricia Rudolph",
+    Notes: "NOTE: Has a cat, dirtiest room on the floor.",
+    Dining: "Dining Preference: Sometimes eats in the room"
+  },
+
+  "1305": {
+    title: "ROOM 1305",
+    Resident: "RESIDENT NAME: Nancy Dalton",
+    Notes: "NOTE: Almost deaf, cannot standup, needs help moving.",
+    Dining: "Dining Preference: Eats downstairs"
+  },
+
+  "1306": {
+    title: "ROOM 1306",
+    Resident: "RESIDENT NAME: Bob",
+    Notes: "NOTE: Has a cat, but room is well kept.",
+    Dining: "Dining Preference: Eats downstairs"
+  },
+
+  "1307": {
+    title: "ROOM 1307",
+    Resident: "RESIDENT NAME: Ray Tomasini",
+    Notes: "NOTE: Garbage in the bathroom needs to be taken out more than once a week.",
+    Dining: "Dining Preference: Eats downstairs"
+  },
+
+  "1308": {
+    title: "ROOM 1308",
+    Resident: "RESIDENT NAME: Unknown",
+    Notes: "NOTE: Bathroom gets very dirty",
+    Dining: "Dining Preference: Sometimes eats in the room"
+  },
+
+  "1309": {
+    title: "ROOM 1309",
+    Resident: "RESIDENT NAME: Unknown",
+    Notes: "NOTE: Prefers no housekeeping",
+    Dining: "Dining Preference: Eats downstairs"
   },
 
   "Kitchen": {
@@ -36,8 +94,9 @@ rooms.forEach(room => {
 
     alert(
       `${data.title}\n` +
-      `${data.description}\n` +
-      `Status: ${data.status}`
+      `${data.Resident || ''}\n` +
+      `${data.Notes || ''}\n` +
+      `${data.Dining || ''}\n`
     );
 
   });
